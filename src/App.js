143613b42441a803f79df94420f2449e9a1ac2bb
Anton <tkopernikov63@msil.ru>
2023-02-23 20:49:48 +0300

    редакс, слайдер, шрифты

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import React from 'react';
+import { connect} from 'react-redux'
+
+
+function App(props) {
+    return (
+        <div className = "App" >
+        </div>
+    );
 }
 
-export default App;
+const mapStateToProps = (store) => {
+    return {
+        products: store.products
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App)
