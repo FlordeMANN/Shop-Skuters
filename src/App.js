339a67742b2ec35cc9e270514ee069c2aa0d9a64
Anton <tkopernikov63@msil.ru>
2023-03-03 21:22:03 +0300

    Добаление шапки  Header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-
+import Header from './components/header/Header'
 import Content from './components/content/Content';
-
+import Modal from './components/modal/Modal'
 
 import React from 'react';
 import { connect} from 'react-redux'
-
+import Footer from './components/footer/Footer';
+import ScrollArrow from './components/ScrollArrow/ScrollArrow';
 
 function App(props) {
     return (
         <div className = "App" >
+            <Header />
+            <Modal />
             <Content />
+            <Footer />
+            <ScrollArrow />
         </div>
     );
 }
