f44f04d8192bc4dce8e1e04daf4f20df891d3d6f
Anton <tkopernikov63@msil.ru>
2023-04-12 23:37:36 +0300

    Добавлениe формы регистрации для пользователей

---
+++ src/components/registers/registers.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { auth } from '../firebase';
+
+const Register = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleRegister = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(email, password);
+      console.log(user);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <form onSubmit={handleRegister}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" value={email} onChange={handleEmailChange} />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" value={password} onChange={handlePasswordChange} />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
+
+export default Register;
