98d002a6900e00368bc760fb9679e087ce2a5c28
Anton <tkopernikov63@msil.ru>
2023-04-26 00:42:18 +0300

    Импорт  Firebase в свой проект

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
-
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 function Modal() {
     const initialState = {
@@ -13,6 +14,41 @@ function Modal() {
         const {name, value} = e.target
         setCall({...call, [name]: value });
     };
+    
+    const handleSend = () => {
+        // Инициализация приложения Firebase
+        const firebaseConfig = {   
+        apiKey: "AIzaSyCQamvcZ6WMStzCyLHkw1fSJ5IOYB2g0vE",
+        authDomain: "shops-e24b4.firebaseapp.com", 
+        databaseURL: "https://shops-e24b4-default-rtdb.firebaseio.com",
+        projectId: "shops-e24b4", 
+        storageBucket: "shops-e24b4.appspot.com", 
+        messagingSenderId: "766763700730",
+        appId: "1:766763700730:web:d85f9fd90dc87e0783d443",
+        measurementId: "G-QDG3JX5KMS"
+        };
+        firebase.initializeApp(firebaseConfig);
+        
+        // Ссылка на коллекцию в Firestore
+        const db = firebase.firestore();
+        const collectionRef = db.collection("feedback");
+        
+        // Добавление документа в коллекцию с данными из формы
+        collectionRef.add({
+            phone: call.phone,
+            mail: call.mail,
+            address: call.address,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        })
+        .then(() => {
+            console.log("Заявка отправлена");
+            setCall(initialState); // Сброс формы после отправки
+        })
+        .catch((error) => {
+            console.error("Ошибка отправки заявки: ", error);
+        });
+    };
+    
     return (
         <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div className="modal-dialog" role="document">
@@ -30,13 +66,4 @@ function Modal() {
                     <input className="form-control my-1" type="text" name="address" placeholder="Адрес" onChange={(e) => handleChange(e)}/>
                 </div>
                 <div className="modal-footer">
-                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Закрыть</button>
-                    <button type="button" className="btn btn-primary" data-dismiss="modal">Отправить</button>
-                </div>
-                </div>
-            </div>
-        </div>
-    )
-}
-
-export default Modal
+                    <button type="button" className="btn btn-secondary" data-dismiss="modal">З
